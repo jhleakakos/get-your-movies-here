61e2a1a60754c169bdbc554c4fa9168d33d6c48f
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2021-04-02 15:06:57 -0600

    initial commit; includes basic express app that serves up index.html

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, () => console.log('express listening on port 3000'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'views') });
+})
