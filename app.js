81ee3d05d41b4850e0a5eb549ec6e81bf9908294
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2021-04-02 15:20:47 -0600

    added in ejs

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejs = require('ejs');
 
 app.listen(3000, () => console.log('express listening on port 3000'));
+app.engine('ejs', ejs);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, 'views') });
