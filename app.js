ff224cb7147a11e34d64066118762b0ece1ba74c
Jeremy Leakakos <jeremy@Jeremys-MacBook-Pro.local>
2021-04-02 17:14:36 -0400

    Initial css directory

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(3000, () => console.log('express listening on port 3000'));
 
 app.get('/', (req, res) => {
