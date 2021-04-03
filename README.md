167c28133628dde0aa663af5cd420b360847fca4
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2021-04-02 20:22:56 -0600

    added in proposed REST routes in comments in app.js

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Sample movie rental app
 
 
 
-TV data sets come from the [API](https://www.tvmaze.com/api) from [TVMaze](https://www.tvmaze.com). The data is licensed under Creative Commons license [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0). Please note that the license for the data set is different than that for the app. Please take appropriate measures to follow the licensing for the data set.
+TV data sets come from the [API](https://www.tvmaze.com/api) from [TVMaze](https://www.tvmaze.com). The data is licensed under Creative Commons license [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0). Please note that the license for the data set is different than that for the app. Please take appropriate measures to follow the licensing for the data sets.
--- app.js
+++ app.js
@@ -12,3 +12,52 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, 'views') });
 })
+
+
+
+
+
+
+
+
+
+/*
+REST routes
+
+GET     /                 landing page
+
+GET     /movies           get main page listing of movies
+POST    /movies           add movie
+GET     /movies/new       form to add a new movie
+GET     /movies/:id       show page for individual movie
+PATCH   /movies/:id       edit movie
+GET     /movies/:id/edit  form to edit to movie
+DELETE  /movies/:id       delete movie
+
+GET     /shows            get main page listing of tv shows
+POST    /shows            add tv show
+GET     /shows/new        form to add new tv show
+GET     /shows/:id        show page for individual tv show
+PATCH   /shows/:id        edit show
+GET     /shows/:id/edit   form to edit tv show
+DELETE  /shows/:id        delete show
+
+POST    /review           add review
+DELETE  /review           delete review
+
+POST    /comment          add comment
+DELETE  /comment          delete comment
+
+GET     /login            login page
+GET     /logout           log out user
+GET     /register         form to register user account
+POST    /register         add user
+
+GET     /user             main landing page for users
+GET     /user/:id         show page for individual user
+POST    /user/:id         edit user
+GET     /user/:id/edit    form to edit user
+
+GET     /admin            admin page
+
+*/
