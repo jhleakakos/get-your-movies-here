2a9c762e381034e3286d0ea88c70078ad2dbd872
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2021-04-02 21:08:59 -0600

    switched index page to ejs filetype; added test data in index.ejs and a little to distinguish parts of page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, 'views') });
+    res.render('index');
 })
 
 
