ad0310ac7557b839e5978244454b533bd1fd777c
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2021-09-13 22:32:23 -0600

    add loop through show reviews in view; populate reviews in route for individual tv show

--- routes/shows.js
+++ routes/shows.js
@@ -14,7 +14,7 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
-    const show = await Show.findById(id);
+    const show = await Show.findById(id).populate('reviews');
     res.render('shows/show', { show });
 })
 
