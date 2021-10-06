6b7edfe87bbc94145f502ba8185a57e125ff8a75
Joseph Leakakos <49359469+jhleakakos@users.noreply.github.com>
2021-10-05 20:52:52 -0600

    change search query from exact match to regex search with wildcards before and after search term

--- routes/search.js
+++ routes/search.js
@@ -4,11 +4,9 @@ const Movie = require('../models/movie');
 const Show = require('../models/show');
 
 router.post('/', async (req, res) => {
-    const search = req.body.search;
-    const movies = await Movie.find({ name: search.toString() });
-    const shows = await Show.find({ name: search.toString() });
-    console.log(movies);
-    console.log(shows);
+    const search = req.body.search.toString();
+    const movies = await Movie.find({ name: { $regex: search, $options: 'i' }});
+    const shows = await Show.find({ name: { $regex: search, $options: 'i' }});
     res.render('search', { search, movies, shows });
 })
 
